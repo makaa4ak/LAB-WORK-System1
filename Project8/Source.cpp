8496baad46129759c5777c12178bdfa7149cbfe6
Maks <ganichmaks6@gmail.com>
2024-02-07 16:29:46 +0200

    Add project files.

---
+++ Project8/Source.cpp
@@ -0,0 +1,90 @@
+#include "Windows.h"
+#include "windowsx.h"
+#include "tchar.h"
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	WCHAR className[] = L"FirstWindow";
+
+	WNDCLASSEX wndClass;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	wndClass.hInstance = hInstance;
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hIconSm = NULL;
+	wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = className;
+
+	wndClass.lpfnWndProc = WindowProc;
+
+	if (!RegisterClassEx(&wndClass)) {
+		return 0;
+	}
+
+	HWND hWnd = CreateWindowEx(
+		0,
+		className,
+		L"Window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, //x,y
+		CW_USEDEFAULT, CW_USEDEFAULT, //width, height
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hWnd, nShowCmd);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+VOID CALLBACK MoveWindow(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
+{
+	RECT rect;
+	GetWindowRect(hwnd, &rect);
+
+	int width = rect.right - rect.left;
+	int height = rect.bottom - rect.top;
+
+	MoveWindow(hwnd, rect.left + 10, rect.top, width, height, true);
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0); 
+		break;
+
+	case WM_KEYDOWN:
+		if (wParam == VK_RETURN)
+		{
+
+			MoveWindow(hwnd, 0, 0, 300, 300, true);
+			SetTimer(hwnd, 1, 100, MoveWindow);
+
+		}
+		else if (wParam == VK_ESCAPE)
+			KillTimer(hwnd, 1);
+		break;
+
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+
+	return 0;
+}
